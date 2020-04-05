4e1cdc926395037a770be849d480794a03bc5926
Fabrizio Manna <mannafabrizio83@gmail.com>
2020-04-06 00:08:05 +0200

    UPDATE GENERAL
    UPDATE GENERAL

--- app/api/data/schema/schema.js
+++ app/api/data/schema/schema.js
@@ -2,47 +2,47 @@
     const mongoose = require('mongoose');
     const { Schema } = mongoose;
 // EXPORTING MODULES WITH SCHEMA
-module.exports = {
-    users: new Schema({
-        admin: 
-        {
-            type: Boolean,
-            required: true,
-            default: false
-        },
-        email:
-        {
-            type: String,
-            required: true
-        },
-        password:
-        {
-            type: String,
-            required: true 
-        },
-        username: 
-        {
-            type: String,
-            required: true
-        },
-        name: 
-        {
-            type: String,
-            required: true
-        },
-        surname: 
-        {
-            type: String,
-            required: true
-        },
-        confirmed: 
-        {
-            type: Boolean,
-            required: true,
-            default: false
-        },
-        create: { type: Date },
-        updated: { type: Date, default: Date.now, required: true },
-        modified: { type: Date, default: Date.now, required: true },
-    }),
-};
+    module.exports = {
+        users: new Schema({
+            admin: 
+            {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            email:
+            {
+                type: String,
+                required: true
+            },
+            password:
+            {
+                type: String,
+                required: true 
+            },
+            username: 
+            {
+                type: String,
+                required: true
+            },
+            name: 
+            {
+                type: String,
+                required: true
+            },
+            surname: 
+            {
+                type: String,
+                required: true
+            },
+            confirmed: 
+            {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            create: { type: Date },
+            updated: { type: Date, default: Date.now, required: true },
+            modified: { type: Date, default: Date.now, required: true },
+        }),
+    };
