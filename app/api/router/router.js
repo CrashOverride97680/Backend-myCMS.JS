4ed3e84213eab8bb8edb513525239275ba3f16bc
Fabrizio Manna <mannafabrizio83@gmail.com>
2020-04-06 01:46:28 +0200

    UPDATE GENERAL STYLE CODE
    UPDATE GENERAL STYLE CODE

--- app/api/router/router.js
+++ app/api/router/router.js
@@ -1,27 +1,26 @@
 // IMPORT MODULES NODEJS
-    const express = require('express');
-    const router = express.Router();
-    const controller = require('../controller/controller');
-    const auth = require('../autentication/auth');
-    const isValid = require('../autentication'); 
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/controller');
+const auth = require('../autentication/auth');
+const isValid = require('../autentication');
 // ROUTES APP
 // -> GET
 // -> POST
-    router
-    .post('/login', auth.userLoginValidator, isValid.runValidation, controller.login)
-    .post('/register', auth.userCreateValidator, isValid.runValidation, controller.register)
-    .post('/logout', controller.logout)
-    .post('/posts', controller.createPost);
+router
+	.post('/login', auth.userLoginValidator, isValid.runValidation, controller.login)
+	.post('/register', auth.userCreateValidator, isValid.runValidation, controller.register)
+	.post('/logout', controller.logout)
+	.post('/posts', controller.createPost);
 // -> PUT
 // -> DELETE
 // -> TEST
-if ( process.env.NOD_ENV_TEST )
-    router
-    .get('/test', controller.test)
-    .get('/secret', controller.requireSignin, controller.secretTest)
-    .get('/testMail', controller.testMail);
+if (process.env.NOD_ENV_TEST)
+	router
+		.get('/test', controller.test)
+		.get('/secret', controller.requireSignin, controller.secretTest)
+		.get('/testMail', controller.testMail);
 // -> 404
-    router
-    .get('*', controller.notFound);
+router.get('*', controller.notFound);
 // EXPORTING ROUTES
-    module.exports = router;
+module.exports = router;
