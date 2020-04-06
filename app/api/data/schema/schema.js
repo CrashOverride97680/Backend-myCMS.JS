e521deffe60592790d1cfbd6e328f223111fabb6
Fabrizio Manna <mannafabrizio83@gmail.com>
2020-04-06 22:15:05 +0200

    UPDATE GENERAL STYLE CODE
    UPDATE GENERAL STYLE CODE

--- app/api/data/schema/schema.js
+++ app/api/data/schema/schema.js
@@ -1,48 +1,48 @@
 // IMPORT MODULES NODEJS
-    const mongoose = require('mongoose');
-    const { Schema } = mongoose;
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 // EXPORTING MODULES WITH SCHEMA
-    module.exports = {
-        users: new Schema({
-            admin: 
-            {
-                type: Boolean,
-                required: true,
-                default: false
-            },
-            email:
-            {
-                type: String,
-                required: true
-            },
-            password:
-            {
-                type: String,
-                required: true 
-            },
-            username: 
-            {
-                type: String,
-                required: true
-            },
-            name: 
-            {
-                type: String,
-                required: true
-            },
-            surname: 
-            {
-                type: String,
-                required: true
-            },
-            confirmed: 
-            {
-                type: Boolean,
-                required: true,
-                default: false
-            },
-            create: { type: Date },
-            updated: { type: Date, default: Date.now, required: true },
-            modified: { type: Date, default: Date.now, required: true },
-        }),
-    };
+module.exports = {
+    users: new Schema({
+        admin:
+        {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        email:
+        {
+            type: String,
+            required: true
+        },
+        password:
+        {
+            type: String,
+            required: true
+        },
+        username:
+        {
+            type: String,
+            required: true
+        },
+        name:
+        {
+            type: String,
+            required: true
+        },
+        surname:
+        {
+            type: String,
+            required: true
+        },
+        confirmed:
+        {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        create: { type: Date },
+        updated: { type: Date, default: Date.now, required: true },
+        modified: { type: Date, default: Date.now, required: true },
+    }),
+};
