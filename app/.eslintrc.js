eb2bb98dab97e0f169741b0f579f7647e854d4fa
Fabrizio Manna <mannafabrizio83@gmail.com>
2020-04-07 18:42:04 +0200

    INIZIALIZE ESLINT WITH AIRBNB
    INIZIALIZE ESLINT WITH AIRBNB

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "airbnb",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2019,
-        "sourceType": "module"
-    },
-    "rules": {
-        "func-names": ["error", "never"]
-    }
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+  },
 };
