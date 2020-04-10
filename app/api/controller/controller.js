47abef05ea58a982cff4a09cea9846c8999f0f40
Fabrizio Manna <mannafabrizio83@gmail.com>
2020-04-10 19:49:11 +0200

    LINT
    LINT

--- app/api/controller/controller.js
+++ app/api/controller/controller.js
@@ -30,19 +30,23 @@ const testUserMail = process.env.NODE_ENV_DEV
 		}
 	: null;
 //  EXPORTING MODULE
-module.exports = {
+module.exports = 
+{
 	// FATTO
-	test: (req, resp) => {
-		resp.json({ 
-			resp: lang.LABEL_JSON_STATUS_NUMBER, 
-			server: lang.LABEL_JSON_STATUS 
-		});
+	test: (req, resp) => 
+	{
+		resp
+			.json({ 
+				resp: lang.LABEL_JSON_STATUS_NUMBER, 
+				server: lang.LABEL_JSON_STATUS 
+			});
 	},
 	// FATTO
-	testMail: (req, resp) => {
+	testMail: (req, resp) => 
+	{
 		smtp
 			.testMail()
-			.then((data) => {
+			.then(data => {
 				let SMTPConfig = smtp.createTransport({
 					host: data.smtp.host,
 					port: data.smtp.port,
@@ -61,8 +65,14 @@ module.exports = {
 						html: smtp.template.register({ username: testUserMail })
 					})
 					.then((rejection) => {
-						if (rejection.accepted) resp.status(200).json(lang.LABEL_ACCEPTED_SMTP);
-						else resp.status(500).json(lang.LABEL_ERROR_SMTP);
+						if (rejection.accepted) 
+							resp
+								.status(200)
+								.json(lang.LABEL_ACCEPTED_SMTP);
+						else
+						resp
+							.status(500)
+							.json(lang.LABEL_ERROR_SMTP);
 					})
 					.catch(() => resp.status(500).json(lang.LABEL_500_HTTP));
 			})
@@ -71,92 +81,152 @@ module.exports = {
 	// FATTO
 	secretTest: (req, resp) => resp.json(lang.LABELL_ACCESS_PAGE),
 	// FATTO
-	checkTokenTest: (req, resp) => {
+	checkTokenTest: (req, resp) => 
+	{
 		const token = req.headers['authorization'];
-		jwt.verify(token, secret, (err, decoded) => 
-		{
-			if(!err)
-				resp.status(200).json({
-					info: lang.LABEL_DECODE_TOKEN_TEST, 
-					message: decoded
-				});
-			else
+		jwt
+			.verify(token, secret, (err, decoded) => 
 			{
-				console.log(lang.LABEL_ERROR_RETURN, err);
-				resp.status(403).json(lang.LABEL_403_HTTP);
-			}
-		});
+				if(!err)
+					resp
+						.status(200)
+						.json({
+							info: lang.LABEL_DECODE_TOKEN_TEST, 
+							message: decoded
+						});
+				else
+				{
+					console.log(lang.LABEL_ERROR_RETURN, err);
+					resp
+						.status(403)
+						.json(lang.LABEL_403_HTTP);
+				}
+			});
 	},
 	// FATTO
-	notFound: (req, resp) => {
-		resp.status(404).json({ 
-			resp: lang.LABEL_JSON_STATUS_NUMBER_NOT_FOUND, server: lang.LABEL_JSON_NOT_FOUND 
-		});
+	notFound: (req, resp) => 
+	{
+		resp
+			.status(404)
+			.json({ 
+				resp: lang.LABEL_JSON_STATUS_NUMBER_NOT_FOUND, server: lang.LABEL_JSON_NOT_FOUND 
+			});
 	},
 	// FATTO MA DA MODIFICARE
-	login: (req, resp) => {
-		try {
+	login: (req, resp) => 
+	{
+		try 
+		{
 			const user = {
 				email: req.body.email,
 				password: req.body.password
 			};
 
-			if (user.email === testUser.email && user.password === testUser.password) {
-				if (process.env.NODE_ENV_DEV) {
-					const { id, username, admin } = testUser;
-					jwt.sign({ _id: id, username, admin }, secret, { expiresIn: '1d' }, (err, token) => {
-						if (err) {
-							console.log(lang.LABEL_ERROR_RETURN, err);
-							resp.status(500).json(lang.LABEL_500_HTTP);
-						} else {
-							resp.cookie('token', token, { expiresIn: '1d' });
-							resp.json({
-								auth: true,
-								token
-							});
-						}
-					});
+			if (user.email === testUser.email && user.password === testUser.password) 
+			{
+				if (process.env.NODE_ENV_DEV) 
+				{
+					const { 
+						id, 
+						username, 
+						admin 
+					} = testUser;
+					jwt
+						.sign({ _id: id, username, admin }, secret, { expiresIn: '1d' }, (err, token) => 
+						{
+							if (err) 
+							{
+								console.log(lang.LABEL_ERROR_RETURN, err);
+								resp
+									.status(500)
+									.json(lang.LABEL_500_HTTP);
+							}
+							else 
+							{
+								resp
+									.cookie('token', token, { expiresIn: '1d' });
+								resp
+									.json({
+										auth: true,
+										token
+									});
+							}
+						});
 				}
-			} else resp.status(403).json(lang.LABEL_403_HTTP);
-		} catch (err) {
+			} 
+			else 
+				resp
+					.status(403)
+					.json(lang.LABEL_403_HTTP);
+		} 
+		catch (err) 
+		{
 			console.log(lang.LABEL_ERROR_RETURN, err);
-			resp.status(500).json(lang.LABEL_500_HTTP);
+			resp
+				.status(500)
+				.json(lang.LABEL_500_HTTP);
 		}
 	},
-	createPost: (req, resp) => {
-		try {
+	createPost: (req, resp) =>
+	{
+		try 
+		{
 			const auth = req.headers['authorization'];
-			if (typeof auth !== 'undefined') {
+			if (typeof auth !== 'undefined') 
+			{
 				jwt
-					.verify(auth, secret, 
-					(err, decode) => {
+					.verify(auth, secret, (err, decode) => 
+					{
 						if (err) 
-							resp.status(403).json(lang.LABEL_403_HTTP);
+							resp
+								.status(403)
+								.json(lang.LABEL_403_HTTP);
 						else 
-							resp.status(201).json(lang.LABEL_201_HTTP);
+							resp
+								.status(201)
+								.json(lang.LABEL_201_HTTP);
 					});
-			} else 
-				resp.status(403).json(lang.LABEL_403_HTTP);
-		} catch (err) {
+			} 
+			else 
+				resp
+					.status(403)
+					.json(lang.LABEL_403_HTTP);
+		} 
+		catch (err) 
+		{
 			console.log(lang.LABEL_ERROR_RETURN, err);
-			resp.status(500).json(lang.LABEL_500_HTTP);
+			resp
+				.status(500)
+				.json(lang.LABEL_500_HTTP);
 		}
 	},
 	// FATTO
-	logout: (req, resp) => {
-		try {
-			resp.clearCookie('token');
-			resp.status(200).json(lang.LABEL_LOGOUT);
-		} catch (err) {
+	logout: (req, resp) => 
+	{
+		try 
+		{
+			resp
+				.clearCookie('token');
+			resp
+				.status(200)
+				.json(lang.LABEL_LOGOUT);
+		} 
+		catch (err) 
+		{
 			console.log(lang.LABEL_ERROR_RETURN, err);
-			resp.status(500).json(lang.LABEL_500_HTTP);
+			resp
+				.status(500)
+				.json(lang.LABEL_500_HTTP);
 		}
 	},
 	// FATTO
 	requireSignin: () => expressJWT({ secret }),
-	register: (req, resp) => {
-		try {
-			const user = {
+	register: (req, resp) => 
+	{
+		try 
+		{
+			const user = 
+			{
 				email: req.body.email,
 				password: req.body.password,
 				username: req.body.username,
@@ -169,24 +239,33 @@ module.exports = {
 			if (process.env.NODE_ENV_DEV) 
 				console.log('USER:', user);
 
-			if (!user.token) {
+			if (!user.token) 
+			{
 				const findUser = mongoose.model('user', 'users');
-				try {
-					findUser.findOne(
+				try 
+				{
+					findUser
+						.findOne({email: user.email}, (error, data) => 
 						{
-							email: user.email
-						},
-						(error, data) => {
-							if (error == null) {
-								if (data != null) {
+							if (error == null) 
+							{
+								if (data != null)
+								{
 									if (data.confirmed === false) 
-										resp.status(202).json(lang.LABEL_RESEND_EMAIL);
-								} else {
-									bcrypt.hash(user.password, 10, (err, hash) => {
-										if (!err) {
-											const createUser = mongoose.model('user', 'users');
-											let dateObj = new Date();
-											createUser.create(
+										resp
+											.status(202)
+											.json(lang.LABEL_RESEND_EMAIL);
+								} 
+								else 
+								{
+									bcrypt
+										.hash(user.password, 10, (err, hash) => 
+										{
+											if (!err) 
+											{
+												const createUser = mongoose.model('user', 'users');
+												let dateObj = new Date();
+												createUser.create(
 												{
 													email: user.email,
 													password: hash,
@@ -195,73 +274,108 @@ module.exports = {
 													surname: user.surname,
 													create: dateObj.toISOString()
 												},
-												(err, result) => {
+												(err, result) => 
+												{
 													if (err == null) 
-														resp.status(201).json(lang.LABEL_201_HTTP);
-												}
-											);
-										}
-									});
+														resp
+															.status(201)
+															.json(lang.LABEL_201_HTTP);
+												});
+											}
+										});
 								}
 							}
-						}
-					);
-				} catch (e) {
+						});
+				} 
+				catch (e) 
+				{
 					console.log(lang.LABEL_ERROR_RETURN, e);
-					resp.status(500).json(lang.LABEL_500_HTTP);
+					resp
+						.status(500)
+						.json(lang.LABEL_500_HTTP);
 				}
 			}
-			else {
+			else 
+			{
 				const token = req.headers['authorization'];
-				jwt.verify(token, secret, (err, decoded) => 
-				{
-					if(!err)
+				jwt
+					.verify(token, secret, (err, decoded) => 
 					{
-						const { id, admin } = decoded;
-						if(admin)
+						if(!err)
 						{
-							const findUser = mongoose.model('user', 'users');
-							findUser.findOne(id, 
-							(error, data) => {
-									if (error == null) {
-										if (data != null) {
-											if (data.confirmed == false) resp.status(202).json(lang.LABEL_RESEND_EMAIL);
-										} else {
-											bcrypt.hash(user.password, 10, (err, hash) => {
-												if (!err) {
-													const createUser = mongoose.model('user', 'users');
-													let dateObj = new Date();
-													createUser.create(
+							const { 
+								id, 
+								admin 
+							} = decoded;
+							if(admin)
+							{
+								const findUser = mongoose.model('user', 'users');
+								findUser
+									.findOne(id, (error, data) => 
+									{
+										if (error == null) 
+										{
+											if (data != null) 
+											{
+												if (data.confirmed == false) 
+													resp
+														.status(202)
+														.json(lang.LABEL_RESEND_EMAIL);
+											} 
+											else 
+											{
+												bcrypt
+													.hash(user.password, 10, 
+													(err, hash) => 
+													{
+														if (!err) 
 														{
-															admin: user.admin,
-															email: user.email,
-															password: hash,
-															username: user.username,
-															name: user.name,
-															surname: user.surname,
-															create: dateObj.toISOString()
-														},
-														(err, result) => {
-															if (err == null) resp.status(201).json(lang.LABEL_201_HTTP);
+															const createUser = mongoose.model('user', 'users');
+															let dateObj = new Date();
+															createUser.create(
+															{
+																admin: user.admin,
+																email: user.email,
+																password: hash,
+																username: user.username,
+																name: user.name,
+																surname: user.surname,
+																create: dateObj.toISOString()
+															},
+															(err, result) => 
+															{
+																if (err == null) 
+																	resp
+																		.status(201)
+																		.json(lang.LABEL_201_HTTP);
+															});
 														}
-													);
-												}
-											});
+													});
+											}
 										}
-									}
-							});
-						} else resp.status(403).json(lang.LABEL_403_HTTP);
-					}	
-					else
-					{
-						console.log(lang.LABEL_ERROR_RETURN, err);
-						resp.status(403).json(lang.LABEL_403_HTTP);
-					}
-				});
+									});
+							} 
+							else 
+								resp
+									.status(403)
+									.json(lang.LABEL_403_HTTP);
+						}	
+						else
+						{
+							console.log(lang.LABEL_ERROR_RETURN, err);
+							resp
+								.status(403)
+								.json(lang.LABEL_403_HTTP);
+						}
+					});
 			}
-		} catch (e) {
+		} 
+		catch (e) 
+		{
 			console.log(lang.LABEL_ERROR_RETURN, e);
-			resp.status(500).json(lang.LABEL_500_HTTP);
+			resp
+				.status(500)
+				.json(lang.LABEL_500_HTTP);
 		}
 	}
 };
