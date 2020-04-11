ba3d2fbd6c3912b7fb13df3747b8cecb82ff9ca8
Fabrizio Manna <mannafabrizio83@gmail.com>
2020-04-11 18:56:55 +0200

    LINT
    LINT

--- app/api/autentication/auth.js
+++ app/api/autentication/auth.js
@@ -3,7 +3,10 @@ const { check } = require('express-validator');
 const minPassword = process.env.PASS_LONG || 6;
 // EXPORTING MODULE
 module.exports = {
-	userLoginValidator: [ check('email').isEmail(), check('password').isLength({ min: minPassword }) ],
+	userLoginValidator: [ 
+		check('email').isEmail(), 
+		check('password').isLength({ min: minPassword }) 
+	],
 	userCreateValidator: [
 		check('email').isEmail(),
 		check('password').isLength({ min: minPassword }),
