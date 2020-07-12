1253738896bfbe0d432119f7fc69de8ddeebeae9
Fabrizio Manna <mannafabrizio83@gmail.com>
2020-07-12 08:55:06 +0200

    UPDATE

--- cypress/integration/CMS/index/index_spec.js
+++ cypress/integration/CMS/index/index_spec.js
@@ -1,4 +1,9 @@
 /// <reference types="Cypress" />
+// VARIABLES FOR TESTING
+const testUser = {
+  username: 'test@testmail.com',
+  password: 'testPassword'
+};
 // TESTS
   describe('CMS -> /', () => {
     describe('Set viewport and route for test', () => {
@@ -6,46 +11,71 @@
         cy
           .viewport('macbook-15');
       })
-  
+
       it('Page respond', () => {
         cy
           .visit('/');
       })
-  
+
     })
-  
+
     describe('Check elements if exist', () => {
-  
+
       it('Element input email', () => {
         cy
           .get('input[placeholder=Email]')
           .should('be.visible');
       })
-  
+
       it('Element input password', () => {
         cy
           .get('input[placeholder=Password]')
           .should('be.visible');
       })
-  
+
       it('Element checkbox session', () => {
         cy
           .get('input#sessionSavedCheckbox[type=checkbox]')
           .should('be.visible')
           .uncheck();
-  
+
         cy
           .get('label[for=sessionSaved]')
           .contains('Save session')
           .should('be.visible');
       })
-  
+
       it('Element button submit', () => {
         cy
           .get('button[type=button]')
           .contains('Submit')
           .should('be.visible');
       })
-    })  
-  
+    })
+
+    describe('Test for autocomplete', () => {
+        it('Username autocomplete', () => {
+          cy
+            .get('input[placeholder=Email]')
+            .should('be.visible')
+            .type(testUser.username);
+        })
+
+        it('Password autocomplete', () => {
+          cy
+            .get('input[placeholder=Password]')
+            .should('be.visible')
+            .type(testUser.password);
+        })
+    })
+
+    describe('Submit form', () => {
+      it('Click button', () => {
+        cy
+          .get('button[type=button]')
+          .contains('Submit')
+          .should('be.visible')
+          .click();
+      })
+    })
   })
