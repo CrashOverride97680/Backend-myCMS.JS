44a5af2760e4427d09c78d9cab1c38288ce63241
root <root@developer-server.us-central1-a.c.dark-caldron-278216.internal>
2020-07-25 11:53:08 +0000

    UPDATE

---
+++ app/api/autentication/whitelist-local/whitelist-local.js
@@ -0,0 +1,49 @@
+// IMPORT MODULES NODEJS
+const FileSync = require('lowdb/adapters/FileSync');
+const low = require('lowdb');
+const adapter = new FileSync(__dirname + '/whitelistToken.json');
+const db = low(adapter);
+//  EXPORTING NODE MODULE
+module.exports = {
+    resetDB_LOCAL: () => {
+        db
+            .defaults({
+                tokens: [],
+            })
+            .write();
+    },
+    removeAll_LOCAL: name => {
+        db
+            .get(name)
+            .remove()
+            .write();
+    },
+    insert_LOCAL: ({ name, data }) => {
+        db
+            .get(name)
+            .push(data)
+            .write();
+    },
+    size_LOCAL: name => {
+        const data = db
+            .get(name)
+            .size()
+            .value();
+        return data;
+    },
+    findCache_LOCAL: ({name, data}) => {
+        const token = db
+            .get(name)
+            .find(data)
+            .value();
+        if(token)
+            return true;
+        else
+            return false;
+    },
+    dbRead_LOCAL: data => {
+        db
+            .get(data)
+            .value()
+    },
+};
