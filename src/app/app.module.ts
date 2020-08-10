a7a792d67d4b02c60391ac94b0bb15d00c2e16d2
Fabrizio Manna <mannafabrizio83@gmail.com>
2020-08-10 13:04:11 +0200

    UPDATE DASHBOARD
    UPDATE DASHBOARD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ChartsModule } from 'ng2-charts';
 import { IndexLoginComponent } from './pages/index-login/index-login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
@@ -75,7 +76,8 @@ import { AddWidgetsComponent } from './pages/add-widgets/add-widgets.component';
     FormsModule,
     HttpClientModule,
     FormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
+// INTERFACE
+interface postsInterface {
+  title: string;
+  type: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +18,73 @@ export class DashboardComponent implements OnInit {
   public mailsubData: number = 800;
   public chatData: string = "12.500";
   public paymentsData: string = "10.500";
+  public socialExist: boolean = true;
+  public earningExist: boolean = false;
+  // Array of different segments in chart
+  lineChartData: ChartDataSets[] = [
+    { data: [ 0, 10000, 5000, 20000, 10000, 15000, 30000, 17500, 35000, 40000 ], label: 'Earnings' }
+  ];
+
+  //Labels shown on the x-axis
+  lineChartLabels: Label[] = ['Jan', 'Mar', 'May', 'June', 'Jul', 'Sept', 'Nov'];
+
+  // Define chart options
+  lineChartOptions: ChartOptions = {
+    responsive: true,
+    legend: { display: false }
+  };
+
+  // Define colors of chart segments
+  lineChartColors: Color[] = [
+
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.1)',
+      borderColor: 'rgba(78,115,222,1)',
+    }
+  ];
+
+  // Set true to show legends
+  lineChartLegend = true;
+
+  // Define type of chart
+  lineChartType = 'line';
+
+  lineChartPlugins = [];
+
+  // events
+  chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  // Doughnut
+  public doughnutChartLabels: Label[] = ['Twitter', 'Facebook', 'Instagram'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100]
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  // Define chart options
+  doughnutChartOptions: ChartOptions = {
+    responsive: true
+  };
+
+  public posts: postsInterface[] = [
+    {
+      title: 'Test page for studies component table',
+      type: 'Post electronics',
+      date: '10/08/2017'
+    },
+    {
+      title: ' 2 Test page for studies component table',
+      type: 'Post electronics',
+      date: '10/08/2018'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
