cb5b09470bbc47340c314dc2d251391c825a8e45
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-03-15 12:26:18 +0100

    UPDATE
    UPDATE

--- app/api/autentication/auth.js
+++ app/api/autentication/auth.js
@@ -58,8 +58,7 @@ module.exports = {
 		check('authorization').notEmpty()
 	],
 	deletePosts: [
-		check('authorization').notEmpty(),
-		check('codPosts').notEmpty(),
+		check('authorization').notEmpty()
 	],
 	deleteImage: [
 		check('authorization').notEmpty(),
--- app/api/controller/controller.js
+++ app/api/controller/controller.js
@@ -1917,6 +1917,28 @@ module.exports =
     }
   },
 // FATTO
+  getPostSingle: (req, resp) => {
+    try {
+      const token = req.headers['authorization'];
+      Promise.all([
+        genFunctions.isValidToken({
+          token,
+          localBlacklist: blkLocal,
+          redisBlacklist: redis
+        }),
+        genFunctions.checkTypeUser({
+          token
+        })
+      ])
+    }
+    catch (err) {
+      console.log(lang.LABEL_ERROR_RETURN, err);
+      resp
+          .status(500)
+          .json(lang.LABEL_500_HTTP);
+    }
+  },
+// FATTO
   getMailSubNumbers: (req, resp) => {
     try
     {
@@ -3589,54 +3611,55 @@ module.exports =
   deletePosts: (req, resp) => {
     try {
       const token = req.headers['authorization'];
-      const _id = req.body.codPosts;
-      
-      Promise.all([
-        genFunctions.isValidToken({
-          token,
-          localBlacklist: blkLocal,
-          redisBlacklist: redis
-        }),
-        genFunctions.checkTypeUser({
-          token
-        })
-      ])
-      .then(result => {
-        const res = result[1];
-        const { admin } = res;
-        if(admin)
-        {
-          const deletePosts = mongoose.model('posts', 'posts');
-          deletePosts.findByIdAndRemove(
-          {
-            _id
-          }, (err, data) => 
-          {
-            if(process.env.NODE_ENV_DEV) {
-              console.log(lang.LANG_DEBUG_RESULT, data);
-              console.log(lang.LANG_DEBUG_ERROR, err);
-            }
+      const _id = req.query.id ? req.query.id : false;
+      if(_id) {
+        Promise.all([
+          genFunctions.isValidToken({
+            token,
+            localBlacklist: blkLocal,
+            redisBlacklist: redis
+          }),
+          genFunctions.checkTypeUser({
+            token
+          })
+        ])
+            .then(result => {
+              const res = result[1];
+              const {admin} = res;
+              if (admin) {
+                const deletePosts = mongoose.model('posts', 'posts');
+                deletePosts.findByIdAndRemove(
+                    {
+                      _id
+                    }, (err, data) => {
+                      if (process.env.NODE_ENV_DEV) {
+                        console.log(lang.LANG_DEBUG_RESULT, data);
+                        console.log(lang.LANG_DEBUG_ERROR, err);
+                      }
 
-            if(data !== null)
+                      if (data !== null)
+                        resp
+                            .status(200)
+                            .json(lang.LABEL_200_HTTP);
+                      else
+                        resp
+                            .status(500)
+                            .json(lang.LABEL_500_HTTP);
+                    });
+              } else
+                resp
+                    .json(lang.LABEL_403_HTTP);
+            })
+            .catch(err => {
+              console.log(lang.LANG_DEBUG_ERROR, err);
               resp
-                .status(200)
-                .json(lang.LABEL_200_HTTP);
-            else
-              resp
-                .status(500)
-                .json(lang.LABEL_500_HTTP);
-          });
-        }
-        else
-          resp
-            .json(lang.LABEL_403_HTTP);
-      })
-      .catch(err => {
-        console.log(lang.LANG_DEBUG_ERROR, err);
+                  .status(err.status)
+                  .json(err.lang);
+            });
+      }
+      else
         resp
-          .status(err.status)
-          .json(err.lang);
-      });
+            .json(lang.LABEL_403_HTTP);
     }
     catch (e) {
       console.log(lang.LABEL_ERROR_RETURN, e);
